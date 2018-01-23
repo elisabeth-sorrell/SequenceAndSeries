379c4d4ff9cafe3ac8d25524dfc6d84f00bfda8b
Elisabeth Sorrell <sorrelle04@gmail.com>
2018-01-23 16:19:10 -0600

    add frontend script and index page

--- frontend.py
+++ frontend.py
@@ -1,17 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sequence_library as seq
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-   num = "7"
+@app.route('/calculate', methods = ['POST'])
+def calculate():
+   num = request.form['num']
    resp = seq.get_fibonacci(num)
    if resp.status_code != 200:
       raise ApiError('Cannot get number: {}'.format(resp.status_code))
    fib_num = resp.json()["fibonacci_number"]
+   answer = "The fibonacci number of " + str(num) + " is " + str(fib_num)
+   return render_template('index.html', answer=answer)
 
-   return render_template('index.html', num=num, fib_num=fib_num)
+@app.route('/')
+def home():
+   return render_template('index.html', answer='')
+
 
 if __name__ == '__main__':
    app.run(port='1112')
